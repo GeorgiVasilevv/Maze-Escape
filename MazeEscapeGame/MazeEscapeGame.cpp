80a66db1241a7dd11ad2852e91298ae37bdb65ba
Georgi Vasilev <Georgi010205@gmail.com>
2024-12-24 19:37:56 +0200

    INIT

---
+++ MazeEscapeGame/MazeEscapeGame.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+/**
+ * Solution to course project # 3
+ * Introduction to programming course
+ * Faculty of Mathematics and Informatics of Sofia University
+ * Winter semester 2023/2024
+ *
+ * @author Georgi Vasilev
+ * @idnumber 0MI0600499
+ * @compiler VC
+ *
+ * <code for maze escape game>
+ */
